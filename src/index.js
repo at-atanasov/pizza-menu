cd1016ee99c1eea7788d79a3c4c5b7fc868124d4
at-atanasov <at.atanasov@gmail.com>
2025-05-01 23:49:39 +0300

    Passing and receiving props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,28 +58,67 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  //const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={11} //when we want to pass a value that is not a string we use javascript mode
+      />
+
+      <Pizza
+        name="Pizza Fungi"
+        ingredients="Tomato, spinach, mushrooms and ricotta cheese"
+        photoName="pizzas/funghi.jpg"
+        price={10}
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name}></img>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We are currently open!");
+  // else alert("Sorry we are closed!");
+
   return (
-    <footer>{new Date().toLocaleTimeString()}. We are currently open</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We are currently open
+    </footer>
   );
 
   // return React.createElement("footer", null, "We are currently open");
-}
-
-function Pizza() {
-  return <h2>Pizza !!! </h2>;
 }
 
 const root = ReactDom.createRoot(document.getElementById("root"));
